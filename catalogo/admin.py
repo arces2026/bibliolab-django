6118da83dacffc537ed289259e6de41af65a729c
arces2026 <marco.fullstack2@gmail.com>
2026-06-09 14:24:06 +0200

    fix: cleaned .git

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Libro
 # Register your models here.
+
+@admin.register(Libro)
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ['titolo', 'autore', 'prezzo', 'disponibile']
+    list_filter = ['disponibile']
+    search_fields = ['autore', 'titolo']
+    list_editable = ['disponibile', 'prezzo']
--- catalogo/models.py
+++ catalogo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Libro(models.Model):
+    titolo = models.CharField(max_length=500)
+    autore = models.CharField(max_length=200)
+    editore = models.CharField(max_length=200)
+    descrizione = models.TextField(blank=True)
+    disponibile = models.BooleanField(default=True)
+    prezzo = models.DecimalField(max_digits=8, decimal_places=2)
+    anno_pubblicazione = models.DateField(blank=True)
+    creato_il = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.titolo} di {self.autore} edito da {self.editore}"
