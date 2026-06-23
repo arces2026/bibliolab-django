b0eae2e4c7ca8e06e827e797cf840a159aea8331
arces2026 <marco.fullstack2@gmail.com>
2026-06-23 17:11:27 +0200

    Esercitazione queryset

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Prodotto
+from .models import Prodotto, Prodotto_new, Cliente, Ordine
 # Register your models here.
 
 @admin.register(Prodotto)
@@ -8,3 +8,24 @@ class ProdottoAdmin(admin.ModelAdmin):
     list_filter = ['disponibile']
     search_fields = ['autore', 'titolo']
     list_editable = ['disponibile']
+
+
+@admin.register(Prodotto_new)
+class Prodotto_newAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'descrizione', 'prezzo', 'categoria', 'quantita']
+    list_filter = ['categoria']
+    search_fields = ['descrizione']
+
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'cognome', 'telefono', 'email', 'indirizzo']
+    list_filter = ['cognome']
+    search_fields = ['cognome']
+
+
+@admin.register(Ordine)
+class OrdineAdmin(admin.ModelAdmin):
+    list_display = ['cliente', 'prodotto', 'quantita', 'totale', 'data_ordine', 'stato']
+    list_filter = ['cliente']
+    search_fields = ['stato']
--- catalogo/models.py
+++ catalogo/models.py
@@ -14,3 +14,55 @@ class Prodotto(models.Model):
     class Meta:
         verbose_name_plural = 'Prodotti' # Forza il plurale nella visualizzazione in /Admin
        
+
+class Prodotto_new(models.Model):
+    nome = models.CharField(max_length=500)
+    descrizione = models.TextField()
+    prezzo = models.DecimalField(max_digits=10,decimal_places= 2)
+    categoria = models.CharField(max_length=200)
+    quantita = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.nome}, {self.descrizione}'
+    
+    class Meta:
+        verbose_name_plural = 'Prodotti_new'
+
+# Definisci le scelte PRIMA del modello
+STATO_ORDINE = [
+    ('in_elaborazione', 'In elaborazione'),
+    ('spedito', 'Spedito'),
+    ('consegnato', 'Consegnato'),
+    ('cancellato', 'Cancellato'),
+]
+
+
+class Ordine(models.Model):
+    cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE, related_name='ordini_clienti')
+    prodotto = models.ForeignKey(Prodotto_new, on_delete=models.CASCADE, related_name='ordini_prodotti')
+    quantita = models.PositiveIntegerField(default=1)
+    totale = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    data_ordine = models.DateTimeField(default='2024-01-01 00:00:00')
+    stato = models.CharField(max_length=50, choices=STATO_ORDINE, default='in_elaborazione')
+    
+    def __str__(self):
+        return f"Ordine #{self.id} - {self.cliente.nome} {self.cliente.cognome}"
+
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=200)
+    cognome = models.CharField(max_length=200, blank=True, default='')
+    email = models.EmailField(blank=True, default='')
+    telefono = models.CharField(max_length=30, blank=True, default='')
+    indirizzo = models.CharField(max_length=100, blank=True, default='')
+    # ordini = models.ForeignKey(Ordine, on_delete=models.CASCADE, related_name='ordini')
+
+    def __str__(self):
+        return f'{self.nome}'
+    
+    class Meta:
+        verbose_name_plural = 'Clienti'
+
+
+
+
